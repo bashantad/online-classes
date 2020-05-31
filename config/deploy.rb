a97615a16c921dd1c739ff84493a03b29e7c03e0
Bashanta Dahal <basantad2@gmail.com>
2020-05-31 10:35:59 -0700

    Deployment file changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :puma_workers,    0
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
+#set :stage,           :staging
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
@@ -18,21 +19,23 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options, {:forward_agent => true, user: fetch(:user)}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
+
+set :branch,               :master
+set :keep_releases,         3
+set :conditionally_migrate, true
 # set :format,        :pretty
 # set :log_level,     :debug
-# set :keep_releases, 5
+
 
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -53,8 +56,15 @@ namespace :deploy do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
-        exit
+#        exit
       end
+    end
+  end
+
+  desc "Symlink database.yml file for production"
+  task :config_symlink do
+    on roles(:app) do
+      execute "cp #{shared_path}/database.yml #{release_path}/config/database.yml"
     end
   end
 
@@ -74,7 +84,10 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
+  after 'deploy:symlink:shared', :config_symlink
   after  :finishing,    :compile_assets
+  after  :finishing,    :config_symlink
+#  after  :finishing,    'deploy:sitemap:refresh'
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
