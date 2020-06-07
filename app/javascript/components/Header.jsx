8a5b3013f701cbb2f4c7cb3fbdb3b262e756a5b4
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-06-07 10:56:55 +0545

    Welcome Page Improvements

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,6 +21,11 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+     btnLogout: {
+        color: 'white',
+        textDecoration: 'none',
+        textTransform: 'capitalize',
+}
 }));
 
 export default function Header() {
@@ -27,15 +35,19 @@ export default function Header() {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
+                    <Hidden mdUp>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                    </Hidden>
                     <Typography variant="h6" className={classes.title}>
                         VCRoom
                     </Typography>
-                    <Link to='/users/sign_out'>
+                    <Button  variant="contained" color="primary" disableElevation startIcon={<ExitToAppIcon />}>
+                        <Link to='/users/sign_out' className={classes.btnLogout}>
                             Logout
-                    </Link>
+                        </Link>
+                    </Button>
                 </Toolbar>
             </AppBar>
         </div>
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,39 +1,98 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 import Header from '../components/Header';
 
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import ClassIcon from '@material-ui/icons/Class';
+import MessageIcon from '@material-ui/icons/Message';
+import CallIcon from '@material-ui/icons/Call';
+
+import '../packs/index.scss'
+
 const Home = () => (
     <div>
-        <Header />
-        <h1 className="display-4">Welcome to virtual classroom</h1>
-        <p className="lead">
-            Search the virtual classes that are running now.
-        </p>
-        <Link
-            to="/classroom/1"
-            className="btn btn-lg custom-button"
-            role="button"
-        >
-            Join class
-        </Link>
-        <br />
-        <Link
-            to="/courses/1/messages"
-            className="btn btn-lg custom-button"
-            role="button"
-        >
-            Join Messages
-        </Link>
-        <br />
-        <Link
-            to="/calls/1"
-            className="btn btn-lg custom-button"
-            role="button"
-        >
-            Join Calls
-        </Link>
-  </div>
+        <Header/>
+        <Container maxWidth="sm" className='welcome'>
+            <div className='welcome-main'>
+                <Typography variant="h4" className='welcome-title'>
+                    VCRoom
+                </Typography>
+
+                <Paper className='welcome-paper'>
+                    <Typography variant="h6" className="lead">
+                        Search the virtual classes that are running now.
+                    </Typography>
+                    <div className='search'>
+                        <div className='searchIcon'>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes='search-main'
+                            inputProps={{'aria-label': 'search'}}
+                        />
+                    </div>
+                </Paper>
+            </div>
+        </Container>
+
+        <Container maxWidth="md">
+            <div className='welcome-buttons'>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    className='welcome-btn'
+                    startIcon={<ClassIcon/>}
+                >
+                    <Link
+                        to="/classroom/1"
+                        className="btn btn-lg custom-button"
+                        role="button"
+                    >
+                        Join class
+                    </Link>
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    className='welcome-btn'
+                    startIcon={<MessageIcon/>}
+                >
+                    <Link
+                        to="/courses/1/messages"
+                        className="btn btn-lg custom-button"
+                        role="button"
+                    >
+                        Join Messages
+                    </Link>
+                </Button>
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    className='welcome-btn'
+                    startIcon={<CallIcon/>}
+                >
+                    <Link
+                        to="/calls/1"
+                        className="btn btn-lg custom-button"
+                        role="button"
+                    >
+                        Join Calls
+                    </Link>
+                </Button>
+            </div>
+        </Container>
+    </div>
 );
 
 export default Home;
