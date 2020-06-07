9c1bc5266086c02f717a61069c71c05ce12c0a4d
Bashanta Dahal <basantad2@gmail.com>
2020-06-07 09:00:40 -0700

    Course title fixes

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :body, :website, :course_for, :price
+  attributes :id, :title, :body, :website, :course_for, :price
   has_one :owner
   has_one :category
   has_many :enrolled_users
