5dc5b9b044915064b88c6644bd30ee22856e41b7
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-07-10 12:30:23 +0545

    Rename

--- app/assets/javascript/theme.js
+++ app/assets/javascript/theme.js
@@ -2,5 +2,8 @@
  *= require libraries/jquery.min.js
  *= require libraries/jquery-migrate.min.js
  *= require libraries/bootstrap.bundle.min.js
+ *= require libraries/bootstrap.bundle.min.js
+ *= require hs-unfold.min.js
+ *= require hs-header.min.js
  *= require_self
  */
