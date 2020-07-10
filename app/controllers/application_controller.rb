98375acd1d4151ef3362d71a53ac4752edb07643
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-07-10 15:14:41 +0545

    Approved/Disapproved Courses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	# layout "theme"
+	layout "theme"
 	CALL_JOIN_URL_PATTERN = /^calls\/\d\/join\/\w+$/
 	REACT_ROUTES = {
 		qualifications: '/users/qualifications'
