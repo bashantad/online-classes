9bfd2a14aca093a408a2a11c14a344ca557cf74c
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-07-12 16:25:57 +0545

    Categories/Assets/Courses List

--- app/assets/javascript/theme.js
+++ app/assets/javascript/theme.js
@@ -2,7 +2,21 @@
  *= require libraries/jquery.min.js
  *= require libraries/jquery-migrate.min.js
  *= require libraries/bootstrap.bundle.min.js
+ *= require libraries/hs.core
  *= require hs-unfold.min.js
  *= require hs-header.min.js
+ *= require hs-mega-menu.min.js
+ *= require hs.slick-carousel
+ *= require slick
  *= require_self
  */
+// initialization of header component
+const header = new HSHeader($('#header')).init();
+
+// initialization of HSMegaMenu component
+const megaMenu = new HSMegaMenu($('.js-mega-menu')).init();
+
+// initialization of slick carousel
+$('.js-slick-carousel').each(function() {
+    const slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
+});
