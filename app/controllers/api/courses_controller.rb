bba6515e258927235e350efdbea72aaf3102a5bd
Bashanta Dahal <basantad2@gmail.com>
2020-07-12 22:01:03 -0700

    get course contents in the course details page

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -19,11 +19,11 @@ class Api::CoursesController < Api::BaseController
 	end
 
 	def show
-		render json: @course, include: ['owner', 'reviews', 'reviews.user']
+		render json: @course, include: ['owner', 'reviews', 'reviews.user', 'course_contents']
 	end
 
 	def enrolled
 		@courses = current_user.enrolled_courses.approved
-		render json: @courses, include: ['owner', 'reviews', 'reviews.user']
+		render json: @courses, include: ['owner', 'reviews', 'reviews.user', 'course_contents']
 	end
 end
